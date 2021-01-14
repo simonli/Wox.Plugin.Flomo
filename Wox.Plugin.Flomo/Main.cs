79fea494108a9b0cc561f1bd47c153773139eaf6
simonli <simonli@live.com>
2021-01-14 11:16:10 +0800

    添加项目文件。

---
+++ Wox.Plugin.Flomo/Main.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Wox.Plugin.Flomo
+{
+    public class Main : IPlugin
+    {
+        private PluginInitContext _context;
+        private string _apiUrl;
+        public void Init(PluginInitContext context)
+        {
+            _context = context;
+            Configuration config = Utils.GetConfig();
+            AppSettingsSection appSection = (AppSettingsSection)config.GetSection("appSettings");
+            _apiUrl = appSection.Settings["ApiUrl"].Value;
+        }
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+            const string ico = @"Images\\icon.png";
+
+
+            if (query.Search.Length == 0)
+            {
+                results.Add(new Result
+                {
+                    Title = "请输入Memo",                    
+                    IcoPath = ico
+                });
+                return results;
+            }
+            else
+            {
+                var q = query.Search;
+                results.Add(new Result
+                {
+                    Title = "回车发送",
+                    SubTitle = $"Memo：{q}",
+                    IcoPath = ico,
+                    Action = this.SendToFlomoFunc(q)
+                });
+                return results;
+            }
+        }
+
+        private Func<ActionContext, bool> SendToFlomoFunc(string text)
+        {
+            return c =>
+            {
+                _context.API.ShowMsg(this.SendToFlomo(text) ? "发送成功" : "发送失败");
+                return false;
+            };
+        }
+
+        private bool SendToFlomo(string text)
+        {
+            try
+            {
+                var jsonData = JsonConvert.SerializeObject(new { content = text });
+                var result = Utils.PostWebRequest(_apiUrl, jsonData);
+                var resultObj = JsonConvert.DeserializeObject<ResultEntity>(result);
+                return resultObj.Code == 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }        
+
+    }
+
+    class ResultEntity
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Wox.Plugin.Flomo/Utils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace Wox.Plugin.Flomo
+{
+    public static class Utils
+    {
+        public static Configuration GetConfig()
+        {
+            //获取调用当前正在执行的方法的方法的 Assembly  
+            var assembly = Assembly.GetCallingAssembly();
+            var path = $"{assembly.Location}.config";
+
+            if (File.Exists(path) == false)
+            {
+                var msg = $"{path}路径下的文件未找到 ";
+                throw new FileNotFoundException(msg);
+            }
+
+            try
+            {
+                var configFile = new ExeConfigurationFileMap {ExeConfigFilename = path};
+                var config = ConfigurationManager.OpenMappedExeConfiguration(configFile, ConfigurationUserLevel.None);
+                return config;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static string PostWebRequest(string postUrl, string paramData)
+        {
+            string responseContent = string.Empty;
+            try
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(paramData); //转化
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(postUrl));
+                webReq.Method = "POST";
+                webReq.ContentType = "application/json;charset=UTF-8";
+                webReq.ContentLength = byteArray.Length;
+                using (Stream reqStream = webReq.GetRequestStream())
+                {
+                    reqStream.Write(byteArray, 0, byteArray.Length);//写入参数
+                                                                    //reqStream.Close();
+                }
+                using (HttpWebResponse response = (HttpWebResponse)webReq.GetResponse())
+                {
+                    //在这里对接收到的页面内容进行处理
+                    using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        responseContent = sr.ReadToEnd().ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return responseContent;
+        }
+    }
+}
